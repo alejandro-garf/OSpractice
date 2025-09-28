64ebd6cb188702e32af0d9b42707f2bbbdfc1acd
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-09-28 11:39:10 -0700

    initial commit

---
+++ OS.cpp
@@ -0,0 +1,8 @@
+// Include headers s stuff works 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <string.h>
