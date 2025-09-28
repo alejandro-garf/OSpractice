b19123d1efed861898c7c684d26d2c9c23f22b70
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-09-28 12:11:00 -0700

    second setup

--- OS.cpp
+++ OS.cpp
@@ -6,3 +6,23 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <string.h>
+
+
+#define BUFFER_SIZE 10
+
+typedef struct 
+{
+    int buffer[BUFFER_SIZE];
+    int in;
+    int out;
+} shared_data;
+
+int main()
+{
+    int fd = shm_open("/my_shm", O_CREAT | O_RDWR, 0666);
+    ftruncate(fd, sizeof(shared_data));
+    shared_data *data = (shared_data*) mmap(0, sizeof(shared_data),
+                          PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    
+
+}
