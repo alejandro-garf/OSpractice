d69bc6d94beb804cebd97fa9b6ea05cf331b96e8
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-10-21 07:04:18 +0000

    creating and deleting message queue

--- messenger/queue.cpp
+++ messenger/queue.cpp
@@ -11,11 +11,28 @@ int main()
 
      struct mq_attr attr;
      attr.mq_flags = 0;
-     attr.mq_mxmsg = 10;
+     attr.mq_maxmsg = 10;
      attr.mq_msgsize = 128;
-     attr.mq_curmsg = 0;
+     attr.mq_curmsgs = 0;
 
+     //creating the queue
+     mqd_t qid = mq_open(NAME, O_CREAT | O_EXCL | O_RDWR, 0600, &attr);
+     if (qid == (mqd_t)-1)
+     {
+        perror("Creating the queue failed");
+        return 1;
+     }
 
+     std::cout << "The queue has been created succesfully!\n";
+
+     mq_close(qid);
+     if (mq_unlink(NAME) == -1)
+     {
+        perror("The unlinking has failed");
+        return 2;
+     }
+
+     std::cout << "The message queue has been deleted!\n";
 
     return  0;
 }
