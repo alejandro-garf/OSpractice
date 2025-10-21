adfd90a745648ad3457b2032c022297d55dbb12c
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-10-21 07:47:03 +0000

    receiver is done and connected

--- messenger/receiver.cpp
+++ messenger/receiver.cpp
@@ -6,14 +6,35 @@
 #include <unistd.h>
 
 
-int maiin ()
+int main ()
 {
+    const char* NAME = "/garfqueue"; // name 
 
+    //Opening existinf queue
+    mqd_t qid = mq_open(NAME, O_RDONLY);
+     if (qid == (mqd_t)-1)
+     {
+        perror("Creating the queue failed");
+        return 1;
+     }
 
+     //buffer that will be storing the incoming message
+     char buffer[128];
 
+     //receiving the message 
+     ssize_t pompom = mq_receive(qid, buffer, sizeof(buffer), nullptr);
+     if (pompom == -1)
+     {
+        perror("The message was not received");
+        mq_close(qid);
+        return 2;
+     }
+     
+    std::cout << "The message has been received = " << buffer << std::endl;
+    mq_close(qid);
+    mq_unlink(NAME);
 
+    std::cout << "Everything has been reset\n";
 
-
-    
     return 0;
 }
