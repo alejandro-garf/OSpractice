9081cacf3c9a9d17755eb48c4becff4dee0140da
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-10-21 07:33:53 +0000

    intial commit

---
+++ messenger/receiver.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <mqueue.h>
+#include <fcntl.h>     
+#include <sys/stat.h>
+#include <cstring>
+#include <unistd.h>
+
+
+int maiin ()
+{
+
+
+
+
+
+
+    
+    return 0;
+}
