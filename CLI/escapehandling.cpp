38bab45dcda630ea2c4817f823bfb0d931c3fc99
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-10-22 04:01:37 +0000

    escapehandling.cpp

--- CLI/escapehandling.cpp
+++ CLI/escapehandling.cpp
@@ -1,98 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-
 using namespace std;
-int main()
-{
-    char buffer[8192];
 
-    cout << "Enter your prompt" << endl;
-    cout << "==> ";
+int main() {
+    vector<string> parts = {"echo", "Hello there", "yip \\ \" pee", "yay"};
+    string result = "";
 
-    cin.getline(buffer, 8192);
-
-    string command = buffer;
-    vector<string> parts;
-    string currentPart = "";
-    bool quoting = false; // initally turned off
-    bool escaping = false;
-
-
-    // Loop for the actrual parisng 
-    for (int i = 0; i < command.length(); i++)
-    {
-        char c = command[i];
-
-        if (escaping) 
-        {
-            currentPart += c;
-            escaping = false;
-            continue;
+    for (int i = 0; i < parts.size(); i++) {
+        if (i > 0) {
+            result += " ";
         }
 
-        if (c == '\\')
-        {
-            escaping = true;
-            continue;
+        bool needQuotes = (i > 0);
+        if (needQuotes) {
+            result += "\"";
         }
 
-        if(c == '"')
-        {
-            quoting = !quoting; //turns on quoting mode when "" is detected.
-            continue;
-        }
+        string currentPart = parts[i];
+        for (int j = 0; j < currentPart.length(); j++) {
+            char c = currentPart[j];
 
-        //Handling for when we see a space 
-        if (c == ' ')
-        {
-            if (!quoting) //if quotiing is turned off
-            {
-                if (currentPart.length() > 0)
-                {
-                parts.push_back(currentPart);
-                currentPart = "";
-                }
+            if (c == '"' || c == '\\') {
+                result += "\\";
             }
-            else // if quoting is still turned on.
-            {
-                currentPart += c;
-            }
-        } 
-        else 
-        {
-            currentPart += c;
+
+            result += c;
+        }
+
+        if (needQuotes) {
+            result += "\"";
         }
     }
 
-
-    if (escaping)
-    {
-        perror("backslasha at the end of line!");
-        return 1;
-    }
-
-    if (quoting) // if quoting is still turned on.
-    {
-        perror("You did not add an ending quote!");
-        cout << endl;
-        return 1;
-    }
-
-    if (currentPart.length() > 0)
-    {
-        parts.push_back(currentPart);
-        currentPart = "";
-    }
-
-    cout << "Here is what you typed: " << endl;
-    for (int i = 0; i < parts.size(); i++)
-    {
-        string print = parts[i];
-        cout << print << endl;
-    }
-    
-
+    cout << "Rebuilt command: " << result << endl;
     return 0;
 }
